5a7243d06d91f320faaf28516a2f5e0ecc7f2e4a
sadiZ1 <saadollahahmadi@gmail.com>
2021-10-20 10:12:13 +0200

    begin Create Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SingupComponent } from './auth/singup/singup.component';
+import { SinginComponent } from './auth/singin/singin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './services/auth/auth.component';
+import { BooksComponent } from './services/books/books.component';
+import { AuthGuardComponent } from './services/auth-guard/auth-guard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    SinginComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent,
+    AuthComponent,
+    BooksComponent,
+    AuthGuardComponent
   ],
   imports: [
     BrowserModule
